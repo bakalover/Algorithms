463b8955fa5689ff70764856f9fb0e337bf9fb6e
bakalover <kostya@shprenger.com>
2022-10-23 15:00:18 +0300

    complete

--- ItmoAcademy/graphs/1B.cpp
+++ ItmoAcademy/graphs/1B.cpp
@@ -6,40 +6,28 @@
 using namespace std;
 int main(){
     int t,n,m,u,v;
-    bool check;
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     cin>>t;
     for (size_t i = 0; i < t; i++)
     {
-        check = true;
         cin>>n>>m;
         vector<set<int>> degs;
         degs.resize(n+1);
         for (size_t j = 0; j < m; j++)
         {
             cin>>u>>v;
-            if(u==v){
-                check=false;
-            }
-            size_t u_s = degs.at(u).size();
             degs.at(u).insert(v);
             degs.at(v).insert(u);
-            if (u_s==degs.at(u).size())
-            {
-                check = false;
-            }
             
         }
-        
-        if (check)
+        for (size_t i = 1; i < degs.size(); i++)
         {
-            cout<<"YES"<<endl;
+            cout<<degs.at(i).size()<<" ";
         }
-        else{
-            cout<<"NO"<<endl;
-        }
+        cout<<endl;
+        
     }
     return 0;
 }
