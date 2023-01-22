1f776a6a274cbfe7c845637a45f92e2b9b242eb3
bakalover <kostya@shprenger.com>
2023-01-22 22:15:45 +0300

    A

---
+++ Itmo/suffarr/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    int32_t n;
+    cout << "asda" << endl;
+    cin >> s;
+    return 0;
+}
