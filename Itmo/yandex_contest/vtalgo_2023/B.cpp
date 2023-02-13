373889e63f3a3ce72fd88628062424b7b6c6a43f
bakalover <kostya@shprenger.com>
2023-02-13 22:18:09 +0300

    B...

--- Itmo/yandex_contest/vtalgo_2023/B.cpp
+++ Itmo/yandex_contest/vtalgo_2023/B.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include <stack>
+#include <vector>
 using namespace std;
 
 int main(){
@@ -9,18 +11,47 @@ int main(){
 
     string s;
     cin>>s;
-    for (size_t i = 0; i < s.size()/2; i++)
-    {
-        if(abs((int)s[i] - (int)s[s.size()-1-i]) != 32 && abs((int)s[i] - (int)s[s.size()-1-i]) != 0) {
-            cout<<"Impossible"<<endl;
-            return 0;
+    
+    stack<size_t> an, tr;                             // Stack storing indexes of animals and traps
+    stack<char> all;                                  // Stack for checking intersections (hhhhGGGGggHHhhggHHHH - example for fail without this stack)
+    vector<size_t> indexes((size_t)(s.size()/2));     // Indexes of animals, if answer is "Possible"
+    size_t tr_id{1}, an_id{1};                        // Counters for traps and animals
+
+    for(size_t i = 0; i<s.size();++i){
+        if(all.empty()){
+            all.push(s[i]);
+        }
+
+        if(isupper(s[i])){
+            tr.push(tr_id);
+            ++tr_id;
+        }
+        else{
+            an.push(an_id);
+            ++an_id;
+        }
+        if(!an.empty() && !tr.empty() && tr.top() == toupper(an.top()) && abs((int)all.top() - (int)s[i])==32){
+            indexes[tr.top()-1] = an.top();
+            all.pop();
+            an.pop();
+            tr.pop();
+        }
+        else{
+            all.push(s[i]);
         }
     }
-    cout<<"Possible"<<endl;
-    for (size_t i = 0; i < s.size()/2; i++)
-    {
-        cout<<s.size()/2 - i<<" ";
+    if(an.empty() && tr.empty()){
+        cout<<"Possible"<<endl;
+        for(auto& index: indexes){
+            cout<<index<<" ";
+        }
     }
-    
+    else{
+        cout<<"Impossible"<<endl;
+    }
+
     return 0;
 }
+//ABcCCcbBbbbBBAfffFFFa 
+//hhhhGGGGggHHhhggHHHH
+
