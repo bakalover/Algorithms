427a24bc91d2eaac74c9dbd4cddced4868d0e662
bakalover <kostya@shprenger.com>
2023-02-14 14:51:46 +0300

    done

--- Itmo/yandex_contest/vtalgo_2023/B.cpp
+++ Itmo/yandex_contest/vtalgo_2023/B.cpp
@@ -12,16 +12,12 @@ int main(){
     string s;
     cin>>s;
     
-    stack<size_t> an, tr;                             // Stack storing indexes of animals and traps
-    stack<char> all;                                  // Stack for checking intersections (hhhhGGGGggHHhhggHHHH - example for fail without this stack)
-    vector<size_t> indexes((size_t)(s.size()/2));     // Indexes of animals, if answer is "Possible"
-    size_t tr_id{1}, an_id{1};                        // Counters for traps and animals
+    stack<uint32_t> an, tr;                             // Stack storing indexes of animals and traps
+    stack<char> all;                                    // Stack for checking intersections (hhhhGGGGggHHhhggHHHH - example for fail without this stack)
+    vector<uint32_t> indexes((uint32_t)(s.size()/2));   // Indexes of animals, if answer is "Possible"
+    uint32_t tr_id{1}, an_id{1};                        // Counters for traps and animals
 
     for(size_t i = 0; i<s.size();++i){
-        if(all.empty()){
-            all.push(s[i]);
-        }
-
         if(isupper(s[i])){
             tr.push(tr_id);
             ++tr_id;
@@ -30,17 +26,23 @@ int main(){
             an.push(an_id);
             ++an_id;
         }
-        if(!an.empty() && !tr.empty() && tr.top() == toupper(an.top()) && abs((int)all.top() - (int)s[i])==32){
-            indexes[tr.top()-1] = an.top();
-            all.pop();
-            an.pop();
-            tr.pop();
-        }
-        else{
+
+        if(all.empty()){
             all.push(s[i]);
         }
+        else{
+            if(abs((int)s[i] - (int)all.top())==32){
+                indexes[tr.top()-1] = an.top();
+                all.pop();
+                an.pop();
+                tr.pop();
+            }
+            else{
+                all.push(s[i]);
+            }
+        }
     }
-    if(an.empty() && tr.empty()){
+    if(an.empty() && tr.empty() && all.empty()){
         cout<<"Possible"<<endl;
         for(auto& index: indexes){
             cout<<index<<" ";
@@ -52,6 +54,6 @@ int main(){
 
     return 0;
 }
-//ABcCCcbBbbbBBAfffFFFa 
+//BcCCcbBbbbBBAfffFFFa 
 //hhhhGGGGggHHhhggHHHH
 
