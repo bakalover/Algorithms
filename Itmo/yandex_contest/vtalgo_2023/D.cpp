61e18610b4908ec9381cc69ed28ae4f2c5817e09
bakalover <kostya@shprenger.com>
2023-02-17 11:37:13 +0300

    done

--- Itmo/yandex_contest/vtalgo_2023/D.cpp
+++ Itmo/yandex_contest/vtalgo_2023/D.cpp
@@ -7,11 +7,13 @@ int main(){
     cin.tie(0);
     cout.tie(0);
 
-    int64_t a,b,c,d,k;
+    int64_t a,b,c,d,k,ans{};
 
     bool direction;  
 
     cin>>a>>b>>c>>d>>k;
+
+    ans += a;
     
     // let f(x) = bx - c => f - monotone => (x<f(x)) => (f^p(x) < f^{p+1}(x)) for all p
     // x, f(x), f^2(x), f^3(x), ... -> +inf/-inf (otherwise x = f(x) => f^p(x) = const = a)
@@ -25,14 +27,29 @@ int main(){
     // d_2 = f^2(x) - f(x) = b(a*b - c) - c - (a*b - c) = b(a*b - c - a)
     // d_3 = b^2(a*b - c - a)
     // ... d_n = b^{n-1}(a*b - c - a)
-    // D = sum(d_i) = (1 + b + ... + b^{n-1})*(a*b - c - a) = [(1-b^{n-1})/(1-b)] * (a*b - c - a)
-    // where n - number of experiments
-    // also - changing k to 11 (if too big) : if b>=2, b^11 > 1000 , [[d<1000]]
+    // D = sum(d_i) = (1 + b + ... + b^{n-1})*(a*b - c - a)
     if(direction){ // -> +inf
-        cout<<min((double)d, (double)((double)(1-pow(b,min(k, (int64_t)9)))/(double)(1-b)) * (double)(a*b - c - a) + (double)a);
+        for (size_t i = 0; i < k; i++)
+        {
+            ans += pow(b,i)*(a*b-c-a);
+            if(ans >= d){
+                ans = d;
+                break;
+            }
+        }
+        cout<<ans<<endl;
+        
     }
     else{// -> -inf
-        cout<<max((double)0, (double)((double)(1-pow(b,min(k, (int64_t)9)))/(double)(1-b)) * (double)(a*b - c - a) + (double)a);
+        for (size_t i = 0; i < k; i++)
+        {
+            ans += pow(b,i)*(a*b-c-a);
+            if(ans <= 0){
+                ans = 0;
+                break;
+            }
+        }
+        cout<<ans<<endl;
     }
     return 0;
 }
