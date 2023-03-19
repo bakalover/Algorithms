bc4dae10d9f31c503f851634a4315d98944cab20
bakalover <kostya@shprenger.com>
2023-03-19 15:42:48 +0300

    own sort

--- Itmo/yandex_contest/vtalgo_2023/timus1207.cpp
+++ Itmo/yandex_contest/vtalgo_2023/timus1207.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+//#include <algorithm>
 #include <cmath>
 using namespace std;
+
 
 struct dot
 {
@@ -16,6 +17,50 @@ struct angdot
     size_t i;
     double ang;
 };
+
+void merge(vector<struct angdot> &arr, vector<struct angdot> &help, size_t l, size_t m, size_t r, bool (*func)(struct angdot &p1, struct angdot &p2))
+{
+    size_t i = l;
+    size_t j = m + 1;
+    for (size_t k = l; k <= r; k++)
+    {
+        if (i > m)
+        {
+            arr[k] = help[j++];
+            continue;
+        }
+        if (j > r)
+        {
+            arr[k] = help[i++];
+            continue;
+        }
+        if ((func)(help[j], help[i]))
+        {
+            arr[k] = help[j++];
+        }
+        else
+        {
+            arr[k] = help[i++];
+        }
+    }
+}
+
+void merge_sort(vector<struct angdot> &arr, vector<struct angdot> &help, size_t l, size_t r, bool (*func)(struct angdot &p1, struct angdot &p2))
+{
+    if (l < r)
+    {
+        size_t m = (l + r) / 2;
+        merge_sort(help, arr, l, m, func);
+        merge_sort(help, arr, m + 1, r, func);
+        merge(arr, help, l, m, r, func);
+    }
+}
+
+void sort(vector<struct angdot> &arr, bool (*func)(struct angdot &p1, struct angdot &p2))
+{
+    vector<struct angdot> help(arr);
+    merge_sort(arr, help, 0, arr.size() - 1, func);
+}
 
 int main()
 {
@@ -54,7 +99,7 @@ int main()
     }
     // cout << x_min << " " << y_min << endl;
     // cout << "SIZE:: " << arr.size() << endl;
-    sort(coord_rel_min.begin(), coord_rel_min.end(), [](auto &p1, auto &p2){ return p1.ang < p2.ang; });
+    sort(coord_rel_min, [](auto &p1, auto &p2){ return p1.ang < p2.ang; });
 
     cout << i_min << " " << coord_rel_min[(coord_rel_min.size() - 1) / 2].i << endl;
 
