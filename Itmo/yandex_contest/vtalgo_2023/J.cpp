a7ecf4bb61e4913bb6a7459bce3acf14c6ce3ee9
bakalover <kostya@shprenger.com>
2023-04-08 12:36:47 +0300

    done

---
+++ Itmo/yandex_contest/vtalgo_2023/J.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+class GoblinQueue
+{
+private:
+    deque<T> l, r;
+
+    void rebalance()
+    {
+        if (r.size() < l.size())
+        {
+            r.push_back(l.front());
+            l.pop_front();
+        }
+    }
+
+public:
+    GoblinQueue()
+    {
+    }
+
+    void insert_common(const T el)
+    {
+        l.push_back(el);
+        this->rebalance();
+    }
+
+    void insert_loyal(const T el)
+    {
+        l.push_front(el);
+        this->rebalance();
+    }
+
+    T get_next()
+    {
+        if (r.size() == 0)
+        {
+            T gob = l.front();
+            l.pop_front();
+            return gob;
+        }
+        else
+        {
+            T gob = r.front();
+            r.pop_front();
+            this->rebalance();
+            return gob;
+        }
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    uint32_t n, gob_numb;
+    char type;
+
+    GoblinQueue<uint32_t> queue;
+
+    cin >> n;
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> type;
+        switch (type)
+        {
+        case '-':
+            cout << queue.get_next() << endl;
+            break;
+        case '*':
+            cin >> gob_numb;
+            queue.insert_loyal(gob_numb);
+            break;
+        case '+':
+            cin >> gob_numb;
+            queue.insert_common(gob_numb);
+            break;
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ Itmo/yandex_contest/vtalgo_2023/L.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct segtree
+{
+    vector<int64_t> data;
+    int64_t size;
+
+    void init(int64_t n)
+    {
+        size = 1;
+        while (size < n)
+        {
+            size *= 2;
+        }
+        data.assign(2 * size, 0);
+    }
+
+    void construct(vector<int64_t> &arr)
+    {
+        init(arr.size());
+        construct(arr, 0, 0, size);
+    }
+
+    void construct(vector<int64_t> &arr, int64_t x, int64_t l, int64_t r)
+    {
+        if (r - l == 1)
+        {
+            if (l < arr.size())
+            {
+                data[x] = arr[l];
+            }
+            else
+            {
+                data[x] = 0;
+            }
+        }
+        else
+        {
+            int64_t m = (l + r) / 2;
+            construct(arr, 2 * x + 1, l, m);
+            construct(arr, 2 * x + 2, m, r);
+            data[x] = data[2 * x + 1] < data[2 * x + 2] ? data[2 * x + 1] : data[2 * x + 2];
+        }
+    }
+
+    void set(int64_t i, int64_t v)
+    {
+        set(i, v, 0, 0, size);
+    }
+
+    void set(int64_t i, int64_t v, int64_t x, int64_t l, int64_t r)
+    {
+        if (r - l == 1)
+        {
+            data[x] = v;
+            return;
+        }
+        int64_t m = (l + r) / 2;
+        if (i < m)
+        {
+            set(i, v, 2 * x + 1, l, m);
+        }
+        else
+        {
+            set(i, v, 2 * x + 2, m, r);
+        }
+        data[x] = data[2 * x + 1] < data[2 * x + 2] ? data[2 * x + 1] : data[2 * x + 2];
+    }
+
+    int64_t min(int64_t l, int64_t r)
+    {
+        return min(l, r, 0, 0, size);
+    }
+
+    int64_t min(int64_t l, int64_t r, int64_t x, int64_t lx, int64_t rx)
+    {
+        if (l >= rx || r <= lx)
+        {
+            return 100000000000;
+        }
+        if (lx >= l && rx <= r)
+        {
+            return data[x];
+        }
+        int64_t m = (lx + rx) / 2;
+        int64_t min1 = min(l, r, 2 * x + 1, lx, m);
+        int64_t min2 = min(l, r, 2 * x + 2, m, rx);
+        return min1 < min2 ? min1 : min2;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int32_t n, k, v, ptr = 0;
+
+    cin >> n >> k;
+
+    vector<int64_t> init(n);
+    for (size_t i = 0; i < init.size(); i++)
+    {
+        cin >> init[i];
+    }
+
+    segtree tree;
+
+    tree.construct(init);
+
+    for (int32_t i = 0; i < n - k + 1; i++)
+    {
+        cout << tree.min(i, i + k) << " ";
+    }
+
+    return 0;
+}
