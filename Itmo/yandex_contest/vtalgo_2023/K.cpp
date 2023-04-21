dcb756daabc2cd1833ca0f971b68246be97a0df3
bakalover <kostya@shprenger.com>
2023-04-21 22:43:26 +0300

    suffer but almost

--- Itmo/yandex_contest/vtalgo_2023/K.cpp
+++ Itmo/yandex_contest/vtalgo_2023/K.cpp
@@ -41,6 +41,10 @@ public:
 
     Header *extract_max()
     {
+        if (n == 0)
+        {
+            return nullptr;
+        }
         --n;
         swap(elements[0], elements[n]);
         Header *el = elements.back();
@@ -49,7 +53,7 @@ public:
         return el;
     }
 
-    void update(Header *const el)
+    void update(Header *el)
     {
         siftup(el->heap_index);
     }
@@ -65,6 +69,13 @@ public:
             swap(elements[i], elements[parent(i)]);
             i = parent(i);
         }
+    }
+
+    void del(Header *el)
+    {
+        el->cap = SIZE_MAX;
+        siftup(el->heap_index);
+        this->extract_max();
     }
 
     void siftdown(int32_t i)
@@ -119,132 +130,170 @@ public:
 class Allocator
 {
 private:
-    list<Header> blocks;
-    vector<pair<Header *, bool>> requests;
-    BinHeap free;
+    Header *blocks;
+    vector<pair<Header *, bool>> requests; // true - Проблемы с аллоцированием были (нехватка памяти или уже хотя бы раз деаллоцировали)
+    BinHeap free_queue;
+
+    void fill_block(Header *block, Header *prev, Header *next, int64_t start_pos, size_t cap, size_t heap_index, bool free)
+    {
+        block->prev = prev;
+        block->next = next;
+        block->start_pos = start_pos;
+        block->cap = cap;
+        block->heap_index = heap_index;
+        block->free = free;
+    }
 
 public:
     Allocator(size_t n)
     {
-        blocks.push_back((struct Header){.prev = nullptr, .next = nullptr, .start_pos = 1, .cap = n, .heap_index = 0, .free = true});
-        free.push(&blocks.front());
+        blocks = (Header *)malloc(sizeof(Header));
+        fill_block(blocks, nullptr, nullptr, 1, n, 0, true);
+        free_queue.push(blocks);
     }
 
     int64_t allocate(size_t k)
     {
-        Header *top_el = free.extract_max();
+        Header *top_el = free_queue.extract_max();
+        if (top_el == nullptr)
+        {
+            requests.push_back(make_pair(top_el, true));
+            return -1;
+        }
         if (k > top_el->cap)
         {
             requests.push_back(make_pair(top_el, true));
-            free.push(top_el);
+            free_queue.push(top_el);
             return -1;
         }
         else
         {
-            Header new_block;
             if (k == top_el->cap)
             {
-                new_block = (struct Header){.prev = top_el->prev, .next = nullptr, .start_pos = top_el->start_pos, .cap = k, .heap_index = 0, .free = false};
-                blocks.push_back(new_block);
-                top_el->prev->next = &blocks.back();
-                requests.push_back(make_pair(&blocks.back(), false)); //
+                fill_block(top_el, top_el->prev, top_el->next, top_el->start_pos, k, 0, false);
+                requests.push_back(make_pair(top_el, false));
+                return top_el->start_pos;
             }
             else
             {
-                new_block = (struct Header){.prev = top_el->prev, .next = top_el, .start_pos = top_el->start_pos, .cap = k, .heap_index = 0, .free = false};
-                blocks.push_back(new_block);
+                Header *new_block = (Header *)malloc(sizeof(Header));
+                fill_block(new_block, top_el->prev, top_el, top_el->start_pos, k, 0, false);
                 if (top_el->prev != nullptr)
                 {
-                    top_el->prev->next = &blocks.back();
+                    top_el->prev->next = new_block;
                 }
 
                 top_el->cap -= k;
-                top_el->prev = &blocks.back();
+                top_el->prev = new_block;
                 top_el->start_pos += k;
 
-                free.push(top_el);
+                free_queue.push(top_el);
 
-                requests.push_back(make_pair(&blocks.back(), false));
+                requests.push_back(make_pair(new_block, false));
+                return new_block->start_pos;
             }
-
-            return new_block.start_pos;
         }
     }
 
     void deallocate(int64_t i)
     {
         pair<Header *, bool> cur_pair = requests[i - 1];
+
         Header *cur = cur_pair.first;
-        requests.push_back(cur_pair);
-        if (cur_pair.second)
+        requests.push_back(make_pair(nullptr, false)); // Заполняем мусором (запросы на деаллоцирование учитываются в нумерации)
+
+        if (cur_pair.second) // Не аллоцировано -> делаем ничего
         {
             return;
         }
+
+        requests[i - 1].second = true;
+
         cur->free = true;
+
         if (cur->prev == nullptr && cur->next == nullptr)
         {
-            free.push(cur);
+            fill_block(cur, cur->prev, cur->next, cur->start_pos, cur->cap, 0, true);
+            free_queue.push(cur);
             return;
         }
+
         if (cur->prev == nullptr && cur->next != nullptr)
         {
             if (cur->next->free)
             {
-                cur->next->cap += cur->cap;
-                cur->next->prev = cur->prev;
-                cur->next->start_pos = cur->start_pos;
-                free.update(cur->next);
+                fill_block(cur->next, cur->prev, cur->next->next, cur->start_pos, cur->cap + cur->next->cap, cur->next->heap_index, true);
+                free_queue.update(cur);
+                // free(cur);
+                cur = nullptr;
                 return;
             }
             else
             {
-                free.push(cur);
+                fill_block(cur, cur->prev, cur->next, cur->start_pos, cur->cap, 0, true);
+                free_queue.push(cur);
                 return;
             }
         }
+
         if (cur->prev != nullptr && cur->next == nullptr)
         {
             if (cur->prev->free)
             {
-                cur->prev->cap += cur->cap;
-                cur->prev->next = cur->next;
-                free.update(cur->prev);
+                fill_block(cur->prev, cur->prev->prev, cur->next, cur->prev->start_pos, cur->prev->cap + cur->cap, cur->prev->heap_index, free);
+                blocks = cur->prev;
+                free_queue.update(cur->prev);
+                // free(cur);
+                cur = nullptr;
                 return;
             }
             else
             {
-                free.push(cur);
+                fill_block(cur, cur->prev, cur->next, cur->start_pos, cur->cap, 0, true);
+                free_queue.push(cur);
                 return;
             }
         }
+
         if (cur->prev != nullptr && cur->next != nullptr)
         {
             if (cur->next->free && !cur->prev->free)
             {
-                cur->next->cap += cur->cap;
-                cur->next->prev = cur->prev;
-                cur->next->start_pos = cur->start_pos;
-                free.update(cur->next);
+                fill_block(cur->next, cur->prev, cur->next->next, cur->start_pos, cur->cap + cur->next->cap, cur->next->heap_index, true);
+                fill_block(cur->prev, cur->prev->prev, cur->next, cur->prev->start_pos, cur->prev->cap, 0, false);
+                free_queue.update(cur->next);
+                // free(cur);
                 return;
             }
+
             if (!cur->next->free && cur->prev->free)
             {
-                cur->prev->cap += cur->cap;
-                cur->prev->next = cur->next;
-                free.update(cur->prev);
+                fill_block(cur->prev, cur->prev->prev, cur->next, cur->prev->start_pos, cur->cap + cur->prev->cap, cur->prev->heap_index, true);
+                fill_block(cur->next, cur->prev, cur->next->next, cur->next->start_pos, cur->next->cap, 0, false);
+                free_queue.update(cur->prev);
+                // free(cur);
+                cur = nullptr;
                 return;
             }
+
             if (cur->next->free && cur->prev->free)
             {
-                cur->prev->cap += (cur->cap + cur->next->cap);
-                cur->prev->next = cur->next->next;
-                cur->next->cap = 0;
-                free.update(cur->prev);
+                free_queue.del(cur->prev);
+                fill_block(cur->next, cur->prev->prev, cur->next->next, cur->prev->start_pos, cur->cap + cur->prev->cap + cur->next->cap, cur->next->heap_index, true);
+                if (cur->prev->prev != nullptr)
+                {
+                    cur->prev->prev->next = cur->next;
+                }
+                free_queue.update(cur->next);
+                // free(cur);
+                // free(cur->prev);
                 return;
             }
+
             if (!cur->next->free && !cur->prev->free)
             {
-                free.push(cur);
+                fill_block(cur, cur->prev, cur->next, cur->start_pos, cur->cap, 0, true);
+                free_queue.push(cur);
                 return;
             }
         }
@@ -278,3 +327,61 @@ int main()
 
     return 0;
 }
+
+// 5 100
+// 2
+// 2
+// 1
+// -1
+// -2
+// -3
+// 5
+// 4
+// -7
+// 2
+// 2
+// 1
+// -10
+// -10
+
+// 16 40
+// 4
+// 4
+// 4
+// 4
+// 4
+// 4
+// -1
+// -2
+// 1
+// -9
+// -3
+// -4
+// -5
+// -6
+// 15
+// -15
+// 16
+// -17
+// 10
+// -19
+// 12
+// 4
+// -21
+// 12
+// -22
+// -24
+// 3
+// 5
+// 8
+// -28
+// -27
+// -29
+// 3
+// 5
+// 7
+// -33
+// -35
+// -34
+// 16
+// -39
